bb363d121ecb10d62fb50dbc92c3e468daebe8c9
tinku <rajkgf91@gmail.com>
2024-08-22 16:49:59 +0000

    container config done

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = "65fgg"
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber);
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// "33" =>33
+// "35ees" => NaN 
+// true => 1; false => 0
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
