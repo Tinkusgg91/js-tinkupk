c81a88f46560e3cf4b3383dca75829923ea81a1a
tinku <rajkgf91@gmail.com>
2024-08-22 13:26:49 +0000

    container config done

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";// treat all js code newer version
+// alert("Hello") // We are using nodejs, not browser 
+console.log(3+
+    3); //code readability  should be high
+ console.log("Tinku");
+  
+ let name ="tinku"
+ let age = 18
+ let isLoggedIn = false
+ let state = null
+ // umner => 2 to power 53 rang of intager 
+ // bigint 
+ // string => ""
+ // boolean => true/false
+ // null=> standalone value
+ // undefined =>
+    //symbol => unique 
+ // object 
+
+ console.log(typeof "tinku");
+ console.log(typeof null);
+ console.log(typeof undefined)
